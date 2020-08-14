4bb081093f6a552b5e4d7efa81dd197a23869225
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-08-14 01:47:12 -0500

    major: postprocess

---
+++ src/postprocess.jl
@@ -0,0 +1,36 @@
+using JuliaFormatter
+function postprocess()
+
+  if ENV["CI"] || ENV["CI"] !== "" || !isempty(ENV["CI"]) || !isnothing(ENV["CI"])
+
+    # TODO rewrite using Julia functions
+
+    # Move the content of the directory to the root
+    run(`rsync -a artifact/\* ./`)
+    run(`rm -d -r artifact`)
+
+    # Discard unrelated changes
+    run(`test -f 'Project.toml' \&\& git checkout -- 'Project.toml'`)
+    run(`git ls-files 'Manifest.toml' \| grep . \&\& git checkout -- 'Manifest.toml'`)
+
+    # BUG causes issues
+    # run(`(git diff -w --no-color || git apply --cached --ignore-whitespace && git checkout -- . && git reset && git add -p) || echo done`)
+
+    # Format precompile_includer.jl
+    format_file("./src/precompile_includer.jl")
+
+    # TODO
+    # - name: Create Pull Request
+    #   # https://github.com/marketplace/actions/create-pull-request
+    #   uses: peter-evans/create-pull-request@v2
+    #   with:
+    #     token: ${{ secrets.GITHUB_TOKEN }}
+    #     commit-message: Update precompile_*.jl file
+    #     committer: YOUR NAME <yourEmail@something.com> # NOTE: change `committer` to your name and your email.
+    #     title: "[AUTO] Update precompiles"
+    #     labels: SnoopCompile
+    #     branch: "SnoopCompile_AutoPR"
+
+  end
+
+end
